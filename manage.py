d615bbad3e263af0b0378bfd2f0dfb23c2663e9a
RathishAadiarja <rathishaadi2000@gmail.com>
2022-05-11 02:18:17 +0200

    removed read_dotenv

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     DOT_ENV_PATH = pathlib.Path() /'.env'
     if DOT_ENV_PATH.exists():
         dotenv.read_dotenv(str(DOT_ENV_PATH))
-    dotenv.read_dotenv()
+    #dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todosite.settings')
     try:
         from django.core.management import execute_from_command_line
